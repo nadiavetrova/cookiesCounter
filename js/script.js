9ce7783a3ce596e309a5675dfb018d98c31c8919
nadiavetrova <152676329+nadiavetrova@users.noreply.github.com>
2025-04-20 16:15:07 +0800

    second commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 function counterInit() {
 
+  function getAllCookie() {
+    const cookies = decodeURIComponent(document.cookie);
+    const splitedCookie = cookies.split('; ');
+    const mapedCookie = splitedCookie.map((cookiePair) => {
+      const splitedCookiePair = cookiePair.split('=');
+      let obj = {
+        name: splitedCookiePair[0],
+        value: splitedCookiePair[1]
+      }
+      return obj
+    })
+    return mapedCookie
+  }
+
+  const showAllCookie = getAllCookie();
+  console.log(showAllCookie);
 
 
 
